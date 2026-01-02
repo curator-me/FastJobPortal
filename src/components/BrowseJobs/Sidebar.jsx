f6d6c4a8bdd32305ca40638a9b0dc701de59de75
curator-me <alnoman05405@gmail.com>
2026-01-02 23:25:08 +0600

    added dropdown to job search sidebar

--- src/components/BrowseJobs/Sidebar.jsx
+++ src/components/BrowseJobs/Sidebar.jsx
@@ -1,14 +1,35 @@
 import "./index.css";
 import { keywords } from "../../data/jobLists";
+import { FaAngleDown } from "react-icons/fa6";
+import { useState } from "react";
+import { motion } from "motion/react";
+
+function DropdownIcon({ isOpen }) {
+  return (
+    <motion.div
+      animate={{
+        rotate: isOpen ? 180 : 0,
+        y: isOpen ? 0 : 5,
+      }}
+      transition={{ duration: 0 }}
+    >
+      <FaAngleDown />
+    </motion.div>
+  );
+}
 
 export function Sidebar() {
+  const optionsCount = 5;
+  const [showOptions, setShowOptions] = useState(
+    Array(optionsCount).fill(true)
+  );
   return (
     <aside className="sidebar">
       <h3 className="filter-section-title">Filter Jobs</h3>
 
       {/* Location */}
       <div className="filter-section">
-        <h4>Location</h4>
+        <h4>Location </h4>
         <input
           type="text"
           placeholder="Enter location"
@@ -18,59 +39,103 @@ export function Sidebar() {
 
       {/* Experience */}
       <div className="filter-section">
-        <h4>Experience</h4>
-        <label>
-          <input type="checkbox" />{" "}
-          <span className="checkbox-label">Fresher</span>
-        </label>
-        <label>
-          <input type="checkbox" />{" "}
-          <span className="checkbox-label">1 - 3 Years</span>
-        </label>
-        <label>
-          <input type="checkbox" />{" "}
-          <span className="checkbox-label">5+ Years</span>
-        </label>
+        <h4
+          onClick={() => {
+            setShowOptions((prev) =>
+              prev.map((value, index) => (index === 0 ? !value : value))
+            );
+          }}
+        >
+          Experience
+          <DropdownIcon isOpen={showOptions[0]} />
+        </h4>
+        <div className={showOptions[0] ? "show-options" : "show-options close"}>
+          <label>
+            <input type="checkbox" />{" "}
+            <span className="checkbox-label">Fresher</span>
+          </label>
+          <label>
+            <input type="checkbox" />{" "}
+            <span className="checkbox-label">1 - 3 Years</span>
+          </label>
+          <label>
+            <input type="checkbox" />{" "}
+            <span className="checkbox-label">5+ Years</span>
+          </label>
+        </div>
       </div>
 
       {/* Tags */}
       <div className="filter-section">
-        <h4>Skills / Tags</h4>
-        {keywords.map((keyword, index) => (
-          <label key={index}>
-            <input type="checkbox" />{" "}
-            <span className="checkbox-label">{keyword}</span>
-          </label>
-        ))}
+        <h4
+          onClick={() => {
+            setShowOptions((prev) =>
+              prev.map((value, index) => (index === 1 ? !value : value))
+            );
+          }}
+        >
+          Skills / Tags <DropdownIcon isOpen={showOptions[1]} />
+        </h4>
+        <div className={showOptions[1] ? "show-options" : "show-options close"}>
+          {keywords.map((keyword, index) => (
+            <label key={index}>
+              <input type="checkbox" />{" "}
+              <span className="checkbox-label">{keyword}</span>
+            </label>
+          ))}
+        </div>
       </div>
 
       {/* Employment Type */}
       <div className="filter-section">
-        <h4>Employment Type</h4>
-        <label>
-          <input type="checkbox" />{" "}
-          <span className="checkbox-label">Full Time</span>
-        </label>
-        <label>
-          <input type="checkbox" />{" "}
-          <span className="checkbox-label">Part Time</span>
-        </label>
+        <h4
+          onClick={() => {
+            setShowOptions((prev) =>
+              prev.map((value, index) => (index === 2 ? !value : value))
+            );
+          }}
+        >
+          Employment Type
+          <DropdownIcon isOpen={showOptions[2]} />
+        </h4>
+
+        <div className={showOptions[2] ? "show-options" : "show-options close"}>
+          <label>
+            <input type="checkbox" />{" "}
+            <span className="checkbox-label">Full Time</span>
+          </label>
+          <label>
+            <input type="checkbox" />{" "}
+            <span className="checkbox-label">Part Time</span>
+          </label>
+        </div>
       </div>
       {/* Employment Type */}
       <div className="filter-section">
-        <h4>Environment</h4>
-        <label>
-          <input type="checkbox" />{" "}
-          <span className="checkbox-label">Onsite</span>
-        </label>
-        <label>
-          <input type="checkbox" />{" "}
-          <span className="checkbox-label">Online</span>
-        </label>
-        <label>
-          <input type="checkbox" />{" "}
-          <span className="checkbox-label">Hybrid</span>
-        </label>
+        <h4
+          onClick={() => {
+            setShowOptions((prev) =>
+              prev.map((value, index) => (index === 3 ? !value : value))
+            );
+          }}
+        >
+          Environment
+          <DropdownIcon isOpen={showOptions[3]} />
+        </h4>
+        <div className={showOptions[3] ? "show-options" : "show-options close"}>
+          <label>
+            <input type="checkbox" />{" "}
+            <span className="checkbox-label">Onsite</span>
+          </label>
+          <label>
+            <input type="checkbox" />{" "}
+            <span className="checkbox-label">Online</span>
+          </label>
+          <label>
+            <input type="checkbox" />{" "}
+            <span className="checkbox-label">Hybrid</span>
+          </label>
+        </div>
       </div>
 
       {/* Salary Range */}
