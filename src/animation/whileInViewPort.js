374023b0083d751aad6d4a3239b592179ae59a0b
curator-me <alnoman05405@gmail.com>
2026-01-14 15:40:04 +0600

    added animation in browsejob filter

--- src/animation/whileInViewPort.js
+++ src/animation/whileInViewPort.js
@@ -4,3 +4,9 @@ export const whileInViewPort = {
   transition: { duration: 0.5 },
   viewport: { once: true, margin: "-50px" },
 };
+
+export const getFadeInProps = (index) => ({
+  initial: { opacity: 0, y: 10 },
+  animate: { opacity: 1, y: 0 },
+  transition: { delay: index * 0.03 },
+});
